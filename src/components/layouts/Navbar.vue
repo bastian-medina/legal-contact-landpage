5e071b72207cd7a80f1ae7bdc02daf58382212e5
bastian16 <bastian@escaleno.cl>
2022-11-20 19:35:28 -0300

    fix: q-router-tab to q-router

--- src/components/layouts/Navbar.vue
+++ src/components/layouts/Navbar.vue
@@ -9,16 +9,38 @@
         </q-toolbar>
 
         <q-tabs v-model="tab">
-          <q-route-tab name="inicio" label="Inicio" to="/" />
-          <q-route-tab name="servicios" label="Servicios" to="/services" />
-          <q-route-tab name="sobreNosotros" label="Sobre Nosotros" to="/aboutUs" />
-          <q-route-tab name="contactanos" label="Contactanos" to="/contactUs" />
+          <q-tab name="inicio" label="Inicio" @click="goToIndex()" />
+          <q-tab name="servicios" label="Servicios" to="/services" @click="goToServices()" />
+          <q-tab name="sobreNosotros" label="Sobre Nosotros" to="/aboutUs" @click="goToAboutUs()" />
+          <q-tab name="contactanos" label="Contactanos" to="/contactUs" @click="goToContactUs()" />
         </q-tabs>
       </q-header>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import { useRoute, useRouter } from "vue-router";
 
+//variables
 const tab = ref('inicio')
+const route = useRoute();
+const router = useRouter();
+
+
+//methods
+const goToIndex = () => {
+    router.push("/");
+};
+
+const goToServices = () => {
+    router.push("/services");
+};
+
+const goToAboutUs = () => {
+    router.push("/aboutUs");
+};
+
+const goToContactUs = () => {
+    router.push("/contactUs");
+};
 </script>
